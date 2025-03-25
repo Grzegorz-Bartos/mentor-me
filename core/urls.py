1bfc7b4efe6e0a6773e64a8aacb461403442f4f8
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-03-25 21:40:03 +0100

    feat: add configuration for debug_toolbar

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+from core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += debug_toolbar_urls()
