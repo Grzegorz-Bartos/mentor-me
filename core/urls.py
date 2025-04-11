0942575b0f14e078614b4af39b65fd5922421ddb
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-04-11 19:31:24 +0200

    feat: add AboutView and its url

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from dashboard import views as dashboard_views
 from home import views as home_views
 from listings import views as listings_views
 from mentors import views as mentors_views
@@ -15,6 +16,7 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("listings/", listings_views.ListingListView.as_view(), name="listings"),
     path("mentoring/", mentors_views.MentorsListView.as_view(), name="mentors"),
+    path("about/", dashboard_views.AboutView.as_view(), name="about"),
 ]
 
 
