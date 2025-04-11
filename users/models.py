200d48b449724ba38fb36fb1eeeb28432a70d566
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-04-11 19:59:38 +0200

    feat: add User model

--- users/models.py
+++ users/models.py
@@ -1,1 +1,10 @@
-# Create your models here.
+from django.db import models
+
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    role = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
