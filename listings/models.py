7f2e9b0fe70a5e50f68fc8185872d7f11d35839a
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-04-12 14:15:40 +0200

    feat: add Listing model

--- listings/models.py
+++ listings/models.py
@@ -1,1 +1,18 @@
-# Create your models here.
+from django.db import models
+
+from users.models import User
+
+
+class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.FloatField()
+    location = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    status = models.CharField(max_length=50)  # ex. 'active', 'inactive'
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
