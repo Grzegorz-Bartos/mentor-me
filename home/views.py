46aaeb857318037ef0d0846741a927ac6ab2ce46
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-04-12 16:15:14 +0200

    feat: listings are now rendered dynamically in home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
 
+from listings.models import Listing
+
 
 class HomeView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["listings"] = Listing.objects.order_by("id")[:3]
+        return context
