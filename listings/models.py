4264000adef164c52c7c780f0db93f8dbb578f36
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-04-15 17:01:08 +0200

    refactor: remove location field in listing model and add comments

--- listings/models.py
+++ listings/models.py
@@ -4,15 +4,19 @@ from users.models import Account
 
 
 class Listing(models.Model):
-    user = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="listings")
+    user = models.ForeignKey(
+        Account,
+        on_delete=models.CASCADE,
+        related_name="listings",
+        help_text="User's account",
+    )
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.FloatField()
-    location = models.CharField(max_length=255)
-    category = models.CharField(max_length=100)
-    status = models.CharField(max_length=50)  # ex. 'active', 'inactive'
+    price = models.FloatField()  # -> DecimalField (max_digtis, decimal_places)
+    category = models.CharField(max_length=100)  # choice field
+    status = models.CharField(max_length=50)  # ex. 'active', 'inactive' -> choicefield
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
