32254dcc90f13644c572c7678c63798eb7bc98f3
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-04-17 17:45:25 +0200

    refactor: remove unnecessary fields in Account model which it inherits from AbstractUser

--- users/models.py
+++ users/models.py
@@ -3,9 +3,5 @@ from django.db import models
 
 
 class Account(AbstractUser):
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
     role = models.CharField(max_length=50)
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
